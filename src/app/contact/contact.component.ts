da731f2586b625e57f8a68a11c08e6f4e94da722
Meryem <meryem.ozcelik@gatech.edu>
2019-06-22 01:24:50 -0400

    animations

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,16 +4,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Feedback, ContactType } from '../shared/feedback';
 
+import { flyInOut } from '../animations/app.animation';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 
 export class ContactComponent implements OnInit {
   @ViewChild('fform') feedbackFormDirective;
-    
+
   formErrors = {
     'firstname': '',
     'lastname': '',
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,25 +8,20 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut } from '../animations/app.animation';
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+      visibility(), 
+      flyInOut()
   ]
 })
 export class DishdetailComponent implements OnInit {
