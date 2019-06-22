329559e92bc36e551a656ff2531728759c9b7122
Meryem <meryem.ozcelik@gatech.edu>
2019-06-22 01:03:13 -0400

    visibility animation to dishdetail info

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,11 +8,26 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -25,6 +40,9 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
   dishcopy: Dish;
+  commentForm: FormGroup;
+
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -42,7 +60,7 @@ export class DishdetailComponent implements OnInit {
     }
   };
 
-  commentForm: FormGroup;
+
 
   constructor(private dishservice: DishService,
         @Inject('baseURL') private baseURL,
@@ -59,10 +77,15 @@ export class DishdetailComponent implements OnInit {
        .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
               errmess => this.errMess = <any>errmess); */
+    /* before visibility
     this.route.params
       .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
       .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
         errmess => this.errMess = <any>errmess );
+        */
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+      errmess => this.errMess = <any>errmess);
 
   }
 
